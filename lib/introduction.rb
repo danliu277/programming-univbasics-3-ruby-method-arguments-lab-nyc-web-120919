1383a1f953897f5894db21d4ba74bf26f47e233f
Daniel Liu <danliu277@gmail.com>
2019-11-25 20:35:32 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -12,7 +12,7 @@ def introduction_with_language(name, language)
 end
 
 def introduction_with_language_optional(name, language = "Ruby")
-  print "Hi, my name is Edwin and I am learning to program in Python.\n"
+  print "Hi, my name is #{name} and I am learning to program in #{language}.\n"
 end
 
 
